db507f3c3945ba3e40feda651dc74d45811cc962
brent-thomas <brent@itsnotabugitsafeature.com>
2023-11-03 09:51:43 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Brent's Number Guessing Game
+
+Welcome to Brent's Number Guessing Game, where your intuition and luck can be put to the test! Try to guess the secret number between 1 and 100, and see how many attempts it takes you to get it right.
+
+## How to Play
+
+Run the game, and you'll be greeted with a warm welcome message explaining the rules. Simply enter your guesses and receive hints whether your guess is too high or too low. The game tracks the number of attempts it takes to find the right number and gives you the option to play again with a new number once you've guessed correctly.
+
+## Features
+
+- Intuitive and user-friendly guess submission
+- Helpful hints after each guess
+- High score tracking - try to beat the best score with the least number of attempts
+- Statistical summary of your attempts (mean, median, mode)
+
+## Requirements
+
+This game is built using Python and requires no additional installation of modules - all are part of the Python Standard Library. Make sure you have Python installed on your system to run the game.
+
+## Running the Game
+
+To start the game, navigate to the directory containing the script and run the following command in your terminal:
+
+python guessing_game.py
---
+++ guessing_game.py
@@ -0,0 +1,81 @@
+import random
+import statistics
+import math
+
+# Display Welcome Message
+def welcome_message():
+    print("-" * 36)
+    print("Welcome to Brent's Number Guessing Game!")
+    print(" * I'm thinking of a number between 1 and 100.")
+    print(" * If your guess is too high or too low, I'll let you know. :)")
+    print(" * Good luck!")
+    print("-" * 36)
+
+#Print the high score if there are any previous attempts
+def print_high_score(scores):
+    if len(scores) > 0 :
+        high_score = min(scores)
+        print(f'See if you can beat the high score, {high_score}\n')
+
+
+#Check to see if the guess is correct and provide feedback to the user
+def check_guess(guess, solution):
+    if guess >=1 and guess <=100:
+        if guess > solution:
+            return "It's lower"
+        elif guess < solution:
+            return "It's higher"
+    else:
+        return "Uh oh, that's outside the range. The secret number is between 1 and 100. Try again."
+    
+#Display end of game message, disply score statistics, and prompt player to play again
+def end_of_game_sequence(solution,number_of_attempts,scores):
+        print('*' * 36)
+        print(f"\nBe you angels!? You got it! The correct number was {solution}\n")
+        print(f"You got it right in {number_of_attempts} attempts")
+        print(f'Mean: {statistics.mean(scores)}')
+        print(f'Median: {statistics.median(scores)}')
+        print(f'Mode: {statistics.mode(scores)}')
+        print()
+        print('*' * 36)
+        return input("Would you like to play again? [Y/N] ")
+
+
+
+
+def start_game():
+    welcome_message()
+    scores = []
+    while True:
+        number_of_attempts = 0
+        guess = 0
+        solution = random.randint(1,100)
+        print_high_score(scores)
+        while guess != solution:
+            try:
+                
+                guess = int(input("Make a guess: "))
+                number_of_attempts +=1
+                print(check_guess(guess,solution))
+            except ValueError:
+                print("Please enter a valid whole number")
+        scores.append(number_of_attempts)
+        # print()
+        # print('*' * 36)
+        # print(f"\nBe you angels!? You got it! The correct number was {solution}\n")
+        # print(f"You got it right in {number_of_attempts} attempts")
+        # print(f'Mean: {statistics.mean(scores)}')
+        # print(f'Median: {statistics.median(scores)}')
+        # print(f'Mode: {statistics.mode(scores)}')
+        # print()
+        # print('*' * 36)
+        play_again = end_of_game_sequence(solution,number_of_attempts,scores)
+        if play_again.upper() == 'Y':
+          
+            print("\nGreat! I've picked a new number.How many attempts will it take you this time?\n")
+            continue
+        else:
+            print("\nGoodbye for now!\n\n**Self Destructing in 3,2,1...\n\n")
+            break
+
+start_game()
